0622742c7240aa4881260473c28cebb9243869c0
Philip Levy <philipdlevy@gmail.com>
2022-02-09 13:45:48 -0700

    commit tree c147a0df615d14b87379072aa90eb22a5accf1c5

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function (arrayOfDrivers) {
+    console.log(arrayOfDrivers)
+    return arrayOfDrivers.slice(0,2)
+};
+
+const returnLastTwoDrivers = function(listOfDrivers) {
+    return listOfDrivers.slice(-2)
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+
+const createFareMultiplier = (num) => {
+     return (fair) => {
+        return (num * fair)
+    }
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (drivers, callback) {
+    return callback(drivers);
+};
